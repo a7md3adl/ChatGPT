90ee5705d9c7900bec40efb1f115cb2d77220ed9
Ahmed Adel <a7md.3adl@yahoo.com>
2023-08-22 05:21:28 +0300

    shell navigation updates window tittle

--- ChatGPT/AppShell.xaml.cs
+++ ChatGPT/AppShell.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace ChatGPT;
+﻿using System.Diagnostics;
+
+namespace ChatGPT;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+        this.Navigated += AppShell_Navigated;
 		
 	}
+
+    private void AppShell_Navigated(object sender, ShellNavigatedEventArgs e)
+    {
+        var location = e.Current.Location.ToString().Replace("//","");
+        switch (location.ToLower()) {
+            case "mainpage": Window.Title = "ChatGPT";break;
+            case "bard": Window.Title = "Bard"; break;
+            case "youtube": Window.Title = "YouTube"; break;
+            case "google": Window.Title = "Google"; break;
+            default: Window.Title = "ChatGPT"; break;
+        }
+    }
 }
